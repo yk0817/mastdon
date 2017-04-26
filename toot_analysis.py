e10c1eaec43836d4bece87a4bcd97459f8905a8b
山本憲 <em072029@gmail.com>
2017-04-26 14:51:05 +0900

    形態素

---
+++ toot_analysis.py
@@ -0,0 +1,64 @@
+#coding:utf-8
+import pymysql
+from janome.tokenizer import Tokenizer
+import re
+import sys
+import os
+
+#接続情報
+dbh = pymysql.connect(
+         host='localhost',
+         user='root',
+         password='',
+         db='mastdon',
+         charset='utf8',
+         cursorclass=pymysql.cursors.DictCursor
+    )
+
+# 形態素解析
+#Tokenizer出力例
+# 英国	名詞,固有名詞,地域,国,*,*,英国,エイコク,エイコク
+t = Tokenizer()
+results = []
+r = []
+#カーソル
+stmt = dbh.cursor()
+toot_politic_file = 'toot.wakati'
+
+
+rows = []
+
+cmd = "touch toot.wakati"
+os.system( cmd )
+
+
+for  politic in tokyo_politic_array:
+    sql = "SELECT * FROM toots"
+    stmt.execute(sql)
+    rows = stmt.fetchall()
+    for row in rows:
+        tokens = t.tokenize(row["text"])
+        with open(tokyo_politic_file,'a',encoding='utf-8') as fp:
+            for tok in tokens:
+                if tok.base_form == "*":
+                    w = tok.surface
+                else:
+                    w = tok.base_form
+                ps = tok.part_of_speech #瀕死情報
+                hinsi = ps.split(',')[0]
+                if hinsi in ['名詞','形容詞']:
+                    r.append(w)
+            rl = (" ".join(r)).strip()
+            fp.write(rl)
+            fp.write("\n")
+            r = []
+            rl = ""
+
+
+
+stmt.close();
+dbh.close();
+
+
+
+
