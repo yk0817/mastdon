a4ac3fd69d353430596e24dddafe879f0ab14ce7
山本憲 <em072029@gmail.com>
2017-04-28 22:34:27 +0900

    parse作成中　分解が面倒だからタグごとぶっこむ

--- mast_crawl.rb
+++ mast_crawl.rb
@@ -1,13 +1,41 @@
 require_relative './common.rb'
 
-# スクレイピング先のURL
-url = 'http://www.yahoo.co.jp/'
-charset = nil
-html = open(url) do |f|
-  charset = f.charset # 文字種別を取得
-  f.read # htmlを読み込んで変数htmlに渡す
+
+class MatCrawl
+  def crawl(url)
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset
+      f.read 
+    end
+    Nokogiri::HTML.parse(html, nil, charset)
+  end
+  def parse(nokogiri_parse)
+    doc = nokogiri_parse    
+    doc
+  end
+  
+  
+  def next?
+    #code
+  end
 end
-# htmlをパース(解析)してオブジェクトを生成
-doc = Nokogiri::HTML.parse(html, nil, charset)
-# タイトルを表示
-p doc.title
+
+crawl = MatCrawl.new
+
+while TRUE
+  url = "https://mstdn.jp/@mazzo"
+  nokogiri_parse = crawl.crawl(url)
+  array = []
+  # next_url_param = 
+  nokogiri_parse.css(".entry").each do |parse|
+    hash = {}
+    hash[:boost] = parse.css(".entry-reblog") if parse.at(".entry-reblog")
+    hash[:name] = parse.css(".display-name").to_html
+    hash[:toot_date] = parse.css(".status__relative-time")[0].attributes["title"].value #トゥート日時
+    hash[:text] = parse.css("div.e-content").to_html #テキスト本文 面倒なんでタグごとぶっこむ
+    print(hash)
+    array << hash
+  end
+  sleep(1)
+end
